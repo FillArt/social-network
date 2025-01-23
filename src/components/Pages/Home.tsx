0c597e3e41f77b8179d57efc090e8a54b9824ed6
FillArt <artiom.filipopovschii@gmail.com>
2025-01-22 20:54:44 -0400

    feat: added two styled components responsible for the container

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import {Profile} from "../Profile/Profile";
+import {Navbar} from "../Layout/Navbar/Navbar";
+import {Header} from "../Layout/Header/Header";
+
+import styled from "styled-components";
 
 export const Home = () => {
     return (
-        <div>
-            Home page
-        </div>
+        <>
+         <Container>
+             <Header/>
+         </Container>
+
+         <MainContainer>
+             <Navbar/>
+             <Profile/>
+         </MainContainer>
+        </>
     );
 };
+
+const Container = styled.div`
+    max-width: 1100px;
+    padding: 0 10px;
+    margin: 0 auto;
+`
+
+const MainContainer = styled(Container)`
+    outline: 1px solid red;
+    
+    display: grid;
+    grid-template-columns: 2fr 10fr;
+    gap: 10px;
+`
