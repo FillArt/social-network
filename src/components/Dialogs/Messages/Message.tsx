ff2952f7141cf7569d593c79c174f100ad3e7ceb
FillArt <artiom.filipopovschii@gmail.com>
2025-01-27 23:09:40 -0400

    style: style fot Message components

--- src/components/Dialogs/Messages/Message.tsx
+++ src/components/Dialogs/Messages/Message.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from "styled-components";
 
-export const Message = () => {
+type MessageProps = {
+    myTemp: boolean
+}
+
+export const Message = ({myTemp}: MessageProps) => {
+
     return (
-        <div>
+        <MessageBlock myMessage={myTemp}>
+            <Avatar>
+                <img src="" alt=""/>
+                <span>Name</span>
+            </Avatar>
 
-        </div>
+            <MessageText>
+                <p>Hello World!</p>
+            </MessageText>
+        </MessageBlock>
     );
 };
 
-export default Message;
+
+type MessageBlockStyle = {
+    myMessage: boolean
+}
+
+const MessageBlock = styled.div<MessageBlockStyle>`
+    display: flex;
+    flex-direction: ${({ myMessage }) => (myMessage ? 'row-reverse' : 'row')};
+    justify-content: ${({ myMessage }) => (myMessage ? 'end' : 'start')};
+    gap: 15px;
+    max-width: 300px;
+    margin: ${({ myMessage }) => (myMessage ? '0 0 10px auto' : 'auto 0 10px 0')};
+`
+
+const Avatar = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+    img {
+        background: #282c34;
+        //border-radius: 50%;
+        width: 50px;
+        height: 50px; 
+    }
+`
+
+const MessageText = styled.div`
+    background: #282c34;
+    padding: 8px 25px;
+    max-width: 200px;
+    color: white;
+`
--- src/components/Dialogs/Messages/MessagesList.tsx
+++ src/components/Dialogs/Messages/MessagesList.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Message} from "./Message";
+import styled from "styled-components";
 
 export const MessagesList = () => {
     return (
-        <div>
-            Messages List
-        </div>
+        <Container>
+
+            <Message myTemp={false}/>
+            <Message myTemp={false}/>
+            <Message myTemp={true}/>
+            <Message myTemp={false}/>
+        </Container>
     );
 };
+
+const Container = styled.div`
+    max-width: 500px;
+    width: 100%;
+    margin: 0 auto;
+`
